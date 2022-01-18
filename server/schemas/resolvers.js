162aca8d43db68c7a336948db23972446c0e973e
Lady Gaga <jessiezheqi.li@gmail.com>
2022-01-17 22:19:05 -0800

    Add posts GET

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,15 @@
+const { User, Post } = require("../models");
+
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      //find all
+      return Post.find(params).sort({ createdAt: -1 });
+    },
+    post: async (parent, { _id }) => {
+      //find one
+      return Post.findOne({ _id });
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,33 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Reply {
+    _id: ID
+    replyBody: String
+    createdAt: String
+    username: String
+  }
+  type Post {
+    _id: ID
+    postText: String
+    createdAt: String
+    username: String
+    replyCount: Int
+    replies: [Reply]
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    posts: [Post]
+    friends: [User]
+  }
   type Query {
-    helloWorld: String
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
   }
 `;
 
