d2412f54d99479b5c885829740144ce64ac49937
Lady Gaga <jessiezheqi.li@gmail.com>
2022-01-19 18:14:28 -0800

    Make signup & login take user input

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -25,8 +26,18 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
