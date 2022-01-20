30f944dd280a95f8a5ffdd0d42857d159a6b8691
Lady Gaga <jessiezheqi.li@gmail.com>
2022-01-19 18:24:55 -0800

    Render specifically for My account

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_POSTS } from "../utils/queries.js";
+import { QUERY_POSTS, QUERY_ME_BASIC } from "../utils/queries.js";
+import Auth from "../utils/auth";
+
 import PostList from "../components/PostList/index.js";
+import FriendList from "../components/FriendList";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
+  //render FriendList
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
   const posts = data?.posts || [];
   // console.log(posts);
+
+  const loggedIn = Auth.loggedIn();
+
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
+        <div className={`col-12 mb-3 ${loggedIn && "col-lg-8"}`}>
           {loading ? (
             <div>Loading.....</div>
           ) : (
             <PostList posts={posts} title={"See what is up"}></PostList>
           )}
         </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FriendList
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
+          </div>
+        ) : null}
       </div>
     </main>
   );
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -55,3 +55,44 @@ export const QUERY_USER = gql`
     }
   }
 `;
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      posts {
+        _id
+        postText
+        createdAt
+        replyCount
+        replies {
+          _id
+          createdAt
+          replyBody
+          username
+        }
+      }
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
